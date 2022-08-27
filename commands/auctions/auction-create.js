570dc70b05150a9eb16ff08b449157516be8a9fe
Humblot Stephane <stephane.humblot@iut2.univ-grenoble-alpes.fr>
2022-08-27 07:49:34 +0200

    WIP acreate, weird timestamps

--- commands/auctions/auction-create.js
+++ commands/auctions/auction-create.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { User, Auction } = require('../../models');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -30,6 +31,59 @@ module.exports = {
 				.setDescription('Max number of entries for the auction'),
 		),
 	async execute(interaction) {
-		await interaction.reply('Incomming auction create...');
+		await interaction.deferReply();
+
+		const options = interaction.options;
+		const character = options.getString('character');
+		const img = options.getString('image');
+		const price = options.getInteger('price');
+		const maxUserEntries = options.getInteger('max-user-entries');
+		const maxEntries = options.getInteger('max-entries');
+		const userId = interaction.member.id;
+
+		const startDate = new Date();
+		const startDateTimeStamp = startDate.getTime();
+		const endDate = new Date(startDate);
+		endDate.setDate(startDate.getDate() + 1);
+		const endDateTimeStamp = endDate.getTime();
+
+		const dbUser = await User.findByPk(userId);
+		if (dbUser === null) {
+			await User.create({id: userId});
+		}
+
+		await Auction.create({
+			guild_id: interaction.guildId,
+			user_id: userId,
+			character: character,
+			img_url: img,
+			entry_price: price,
+			max_entries: maxEntries,
+			max_user_entries: maxUserEntries,
+			start_date: startDate,
+			end_date: endDate,
+			created_at: new Date(),
+			updated_at: null,
+			status: 'temp'
+		});
+
+		const user = interaction.client.users.cache.get(userId);
+
+		const embed = new EmbedBuilder()
+			.setColor(0x0099FF)
+			.setTitle("Enchère pour " + character)
+			.setDescription(
+				`Prix d'entrée: ${price}\n` +
+				`Entrées max par participants: ${maxUserEntries}\n` +
+				`Entrées max total: ${maxEntries}`
+			)
+			.addFields(
+				{ name: "Date de début", value: `<t:${startDateTimeStamp}:R><t:${startDateTimeStamp}>`},
+				{ name: 'Date de fin', value: `<t:${endDateTimeStamp}:R><t:${endDateTimeStamp}>`},
+			)
+			.setImage(img)
+			.setFooter({ text: user.username, iconURL: user.avatarURL() });
+
+		await interaction.editReply({ embeds: [ embed ] });
 	},
 };
