fdc181a30dfb4b9624548fa0b0798accf733d438
Stephane Humblot <shumblot@oph74.fr>
2022-08-30 18:47:30 +0200

    Added message events receiver

---
+++ events/message.js
@@ -0,0 +1,50 @@
+const { Guild } = require('../models');
+const { DEFAULT_PREFIX } = require('../config.json');
+
+module.exports = {
+	name: 'message',
+	execute(message) {
+		let prefix = DEFAULT_PREFIX;
+		const client = message.client;
+
+		const guild = Guild.findByPk(message.guildId);
+		if (guild !== null && guild.prefix) {
+			prefix = guild.prefix;
+		}
+
+		if (!message.content.startsWith(prefix) || message.author.bot) {return;}
+
+		const args = message.content.substring(prefix.length).trim().split(/\s+/);
+		const cmdName = args.shift().toLowerCase();
+
+		const command = client.commands.get(cmdName) ||
+        client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
+		if (!command) return;
+
+		if (command.guildOnly && message.channel.type === 'dm') {
+			return message.reply('Cette commande n\'est pas disponible via les DMs!');
+		}
+
+		if (command.permissions) {
+			const authorPerms = message.channel.permissionsFor(message.author);
+			if (!authorPerms || !authorPerms.has(command.permissions)) {
+				return message.reply('Vous n\'avez pas les droits !');
+			}
+		}
+
+		if (command.args && !args.length) {
+			let reply = '';
+			if (command.usage) {
+				reply += `Synatxe: \`${prefix}${command.name} ${command.usage}\`\n${command.description}`;
+			}
+			return message.channel.send(reply);
+		}
+
+		try {
+			command.execute(message, args, client);
+		}
+		catch (error) {
+			message.channel.send(`Syntaxe: \`${prefix}${command.name} ${command.usage}\`\n${command.description}`);
+		}
+	},
+};
