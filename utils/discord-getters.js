99dddc9641ba331888a5ad7551d3aeb896a4c17c
Humblot Stephane <stephane.humblot@iut2.univ-grenoble-alpes.fr>
2022-09-01 23:47:27 +0200

    Missing await

--- utils/discord-getters.js
+++ utils/discord-getters.js
@@ -2,21 +2,21 @@ module.exports = {
     getChannel : async (guild, id) => {
         let channel = guild.channels.cache.get(id);
         if (channel === null) {
-            channel = guild.channels.fetch(id);
+            channel = await guild.channels.fetch(id);
         }
         return channel;
     },
     getMember : async (guild, id) => {
         let member = guild.members.cache.get(id);
         if (member === null) {
-            member = guild.members.fetch(id);
+            member = await guild.members.fetch(id);
         }
         return member;
     },
     getGuild : async (client, id) => {
         let guild = client.guilds.cache.get(id);
         if (guild === null) {
-            guild = client.guilds.fetch(id);
+            guild = await client.guilds.fetch(id);
         }
         return guild;
     }
