871d2ba167b36575e9043ba5094dd9b83b764835
Stephane Humblot <shumblot@oph74.fr>
2022-09-01 20:16:44 +0200

    WIP action-list nav

--- commands/auctions/auction-list.js
+++ commands/auctions/auction-list.js
@@ -1,7 +1,18 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
 const { Auction } = require('../../models');
 const { auctionListEmbed, userAuctionListEmbed } = require('../../utils/auction-list-embed');
 
+const buttonsRow = () => {
+	return new ActionRowBuilder()
+		.addComponents(
+			new ButtonBuilder()
+				.setCustomId('primary')
+				.setLabel('⬅️'),
+			new ButtonBuilder()
+				.setCustomId('primary')
+				.setLabel('➡️'),
+		);
+};
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,6 +22,16 @@ module.exports = {
 		.addUserOption(option =>
 			option.setName('user')
 				.setDescription('User for which to return the list'),
+		)
+		.addStringOption(option =>
+			option.setName('status')
+				.setDescription('Auction Status')
+				.addChoices(
+					{ name: Auction.PENDING_STATUS, value: Auction.PENDING_STATUS },
+					{ name: Auction.ONGOING_STATUS, value: Auction.ONGOING_STATUS },
+					{ name: Auction.CANCELLED_STATUS, value: Auction.CANCELLED_STATUS },
+					{ name: Auction.FINISHED_STATUS, value: Auction.FINISHED_STATUS },
+				),
 		),
 	/**
 	 * TODO: Better list display with navigation, more filters
@@ -18,25 +39,35 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.deferReply();
 		const options = interaction.options;
+		const status = options.getString('status');
 		const member = options.getMember('user');
 		const guild = interaction.guild;
+		const offset = 1;
+		const limit = 5;
 
-		const filter = {where: { guild_id: guild.id }, order: [['start_date', 'DESC']]}
+		const filter = {
+			where: { guild_id: guild.id }, order: [['start_date', 'DESC']],
+			offset: offset,
+			limit: limit,
+		};
 		if (member) {
-			filter.where.user_id = member.id
+			filter.where.user_id = member.id;
+		}
+		if (status) {
+			filter.where.status = status;
 		}
 
 		// Manage it with offset / limit
-		const {count, rows} = await Auction.findAndCountAll(filter);
+		const { count, rows } = await Auction.findAndCountAll(filter);
 
 		if (rows === null || count === 0) {
-			return await interaction.editReply("Aucune enchère retrouvée.");
+			return await interaction.editReply('Aucune enchère retrouvée.');
 		}
 
-		const embed = member 
+		const embed = member
 			? userAuctionListEmbed(count, rows, guild, member)
-			: await auctionListEmbed(count, rows, guild)
-			
-		await interaction.editReply({ embeds: [ embed ] });
+			: await auctionListEmbed(count, rows, guild);
+
+		await interaction.editReply({ embeds: [ embed ], rows: [ buttonsRow() ] });
 	},
 };
