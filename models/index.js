e2aeb633c4ba2bb1951e78fa1a75580a1ac90c9a
Stephane Humblot <SHumblot@oph74.fr>
2023-06-26 15:17:09 +0200

    upd(overall config): better docker-compose, .env and config

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 require('dotenv').config();
 
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config.json')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
